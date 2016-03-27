3fab9e585df36f6b667fd0b455efad242aa3aae4
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-03-26 21:45:35 -0300

    Testando aplicação

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -33,55 +33,67 @@ public class PersonOverviewController {
     private MainApp mainApp;
 
     /**
-     * O construtor.
-     * O construtor é chamado antes do método inicialize().
+     * The constructor.
+     * The constructor is called before the initialize() method.
      */
     public PersonOverviewController() {
     }
 
     /**
-     * PReenche todos os campos de texto para mostrar detalhes sobre a pessoa.
-     * Se a pessoa especificada for null, todos os campos de texto são limpos.
-     * 
-     * @param person a pessoa ou null
-     */
-    private void showPersonDetails(Person person) {
-    	birthdayLabel.setText(DateUtil.format(person.getBirthday()));
-    }
-    
-    /**
-     * Inicializa a classe controller. Este método é chamado automaticamente
-     *  após o arquivo fxml ter sido carregado.
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
      */
     @FXML
     private void initialize() {
-    	 // Inicializa a tabela de pessoas com duas colunas.
+    	// Initialize the person table with the two columns.
         firstNameColumn.setCellValueFactory(
-        cellData -> cellData.getValue().firstNameProperty());
+        		cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(
-        cellData -> cellData.getValue().lastNameProperty());
-
-        // Limpa os detalhes da pessoa.
+        		cellData -> cellData.getValue().lastNameProperty());
+        
+        // Clear person details.
         showPersonDetails(null);
 
-        // Detecta mudanças de seleção e mostra os detalhes da pessoa quando houver mudança.
-        personTable.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> showPersonDetails(newValue));    
+        // Listen for selection changes and show the person details when changed.
+		personTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> showPersonDetails(newValue));
     }
 
     /**
-     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
+     * Is called by the main application to give a reference back to itself.
      * 
      * @param mainApp
      */
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
 
-        // Adiciona os dados da observable list na tabela
+        // Add observable list data to the table
         personTable.setItems(mainApp.getPersonData());
     }
-}/*
-Todos os campos e métodos onde o arquivo fxml precisa de acesso devem ser anotados com @FXML. Na verdade, omente se eles forem private, mas é melhor tê-los private e marcá-los com a anotação!
-O método initialize() é chamado automaticamente após o arquivo fxml ter sido carregado. Nessa hora, todos os campos FXML já devem ter sido inicializados.
-O método setCellValueFactory(...) que nós definimos nas colunas da tabela são usados para determinar qual campo dentro dos objetos de Person devem ser usados para determinda coluna. A seta -> indica que nós estamos usando um recurso do Java 8 chamado Lambdas. (Outra opção seria usar uma PropertyValueFactory, mas esta não é type-safe (segura por tipo)).
-*/
+    
+    /**
+     * Fills all text fields to show details about the person.
+     * If the specified person is null, all text fields are cleared.
+     * 
+     * @param person the person or null
+     */
+    private void showPersonDetails(Person person) {
+    	if (person != null) {
+    		// Fill the labels with info from the person object.
+    		firstNameLabel.setText(person.getFirstName());
+    		lastNameLabel.setText(person.getLastName());
+    		streetLabel.setText(person.getStreet());
+    		postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+    		cityLabel.setText(person.getCity());
+    		birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+    	} else {
+    		// Person is null, remove all the text.
+    		firstNameLabel.setText("");
+    		lastNameLabel.setText("");
+    		streetLabel.setText("");
+    		postalCodeLabel.setText("");
+    		cityLabel.setText("");
+    		birthdayLabel.setText("");
+    	}
+    }
+}
