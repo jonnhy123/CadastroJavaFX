0c6f0bd04d2d179d73bd8106b5ec8b91ae99090b
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-03-27 00:51:19 -0300

    add metodo public void showBirthdayStatistics() a class MainApp

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -13,6 +13,7 @@ import org.controlsfx.dialog.Dialogs;
 
 import ch.makery.address.model.Person;
 import ch.makery.address.model.PersonListWrapper;
+import ch.makery.address.view.BirthdayStatisticsController;
 import ch.makery.address.view.PersonEditDialogController;
 import ch.makery.address.view.PersonOverviewController;
 import ch.makery.address.view.RootLayoutController;
@@ -280,5 +281,31 @@ public class MainApp extends Application {
         }
     }
     
+    /**
+     * Abre uma janela para mostrar as estatísticas de aniversário.
+     */
+    public void showBirthdayStatistics() {
+        try {
+            // Carrega o arquivo fxml e cria um novo palco para o popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/BirthdayStatistics.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Birthday Statistics");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Define a pessoa dentro do controller.
+            BirthdayStatisticsController controller = loader.getController();
+            controller.setPersonData(personData);
+
+            dialogStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
